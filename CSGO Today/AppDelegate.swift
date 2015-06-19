22a244bdeb1cb122b40512104768769a60cb1f27
Casper Storm Larsen <casperstorm@me.com>
2015-06-19 10:12:02 +0200

    Init

---
+++ CSGO Today/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CSGO Today
+//
+//  Created by Casper Storm Larsen on 19/06/15.
+//  Copyright © 2015 Bob. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
